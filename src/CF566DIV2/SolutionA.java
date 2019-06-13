50be8a34fd8d9f333e898e8645ece30a49452055
Vineeth Chitteti <vineeth.chitteti@target.com>
2019-06-13 20:46:15 +0530

    CF566DIV2: 2 problems solved

---
+++ src/CF566DIV2/SolutionA.java
@@ -0,0 +1,16 @@
+package CF566DIV2;
+
+import java.util.Scanner;
+
+public class SolutionA {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n % 2 == 1)
+            System.out.println(0);
+        else {
+            long value = (long)Math.pow(2, n/2);
+            System.out.println(value);
+        }
+    }
+}
---
+++ src/CF566DIV2/SolutionC.java
@@ -0,0 +1,7 @@
+package CF566DIV2;
+
+import java.util.Scanner;
+
+public class SolutionC {
+
+}
